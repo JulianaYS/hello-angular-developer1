d71a32a2feee547d699debc28c90bd7dae001a9b
ArianRM <arianramirezmendoza@gmail.com>
2023-08-31 08:51:21 -0500

    feat(main): Added register developer component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ export class AppComponent {
   title = 'hello Angular Developer Application';
   firstName: string = '';
   lastName: string = '';
+
+  onDeveloperRegistered(developer: { firstName: string, lastName: string }) {
+    this.firstName = developer.firstName;
+    this.lastName = developer.lastName;
+  }
 }
